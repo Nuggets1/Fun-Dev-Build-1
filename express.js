e0ccaa3526f58814f33a4e756a0ccdbb2cd8302f
Nuggets1 <39705515+Nuggets1@users.noreply.github.com>
2018-05-30 02:11:25 +0100

    Added

---
+++ express.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.get("/", (request, response) => {
+
+ console.log(Date.now() + " Just got pinged!");
+
+ response.sendStatus(200);
+
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+
+ http.get(`http://fun-build-1.glitch.me/`);
+
+}, 280000);
